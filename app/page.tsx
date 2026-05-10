054f59665835f050484337c6ee060ba2af9f3fca
Arne Slabbinck <arne.slabbinck@gmail.com>
2026-05-10 19:27:07 +0200

    second try after google maps add

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
 
             {/* Right: Google Maps Embed */}
             <div className="rounded-3xl overflow-hidden shadow-2xl border border-gray-100">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.393503696797!2d2.9629143775339166!3d51.15653443627349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dcade3ed773459%3A0xf8459199d0330a61!2sHoogstraat%2018%2C%208470%20Gistel!5e0!3m2!1sen!2sbe!4v1778433455035!5m2!1sen!2sbe" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2502.393503696797!2d2.9629143775339166!3d51.15653443627349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dcade3ed773459%3A0xf8459199d0330a61!2sHoogstraat%2018%2C%208470%20Gistel!5e0!3m2!1sen!2sbe!4v1778433455035!5m2!1sen!2sbe" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
           </div>
         </div>
